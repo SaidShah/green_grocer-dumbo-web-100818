bcf6fb7c7b1da79f3a91709074e26d35c5aa2176
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 16:37:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -22,9 +22,9 @@ new_cart = {}
   coupons.each do |each_coupon|
     each_coupon.each do |item, values|
       cart.each do |cart_item, item_values|
-        
+        binding.pry
         if item == cart_item 
-          binding.pry
+          
 end
  end  
    end      
