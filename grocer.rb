6caeba5f30f7f764da3525332f9f32f4d5f80056
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 17:04:56 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -24,17 +24,10 @@ new_cart.merge!(cart)
   coupons.each do |each_coupon|
     each_coupon.each do |item, values|
       cart.each do |cart_item, item_values|
-        
         if cart_item == values
           new_cart["#{values} W/COUPON"]={}
           binding.pry 
-           if item == :num 
-              
-             new_cart["#{values} W/COUPON"] = {count => values}
-            
-             new_cart[values][:count]=-values
-           
-         end
+          
  end
   end  
    end      
