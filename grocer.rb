6a2b7d5d31c5b73bec691df5fb62cfcd67cc46ae
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 16:30:38 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -3,27 +3,31 @@ require "pry"
 
 def consolidate_cart(cart)
   cart_hash = {}
-  remove_duplicates = cart.uniq
-  remove_duplicates.each do |each_item|
-    each_item.each {|x,y| cart_hash[x.to_s]=y}
+    remove_duplicates = cart.uniq
+      remove_duplicates.each do |each_item|
+        each_item.each {|x,y| cart_hash[x.to_s]=y}
 end
-
   cart_hash.each {|name, value| cart_hash[name][:count]=0}
-
- cart.each do |each_piece|
-  each_piece.each {|name,stats| cart_hash[name][:count]+=1}
-  
- end
-
- cart_hash
+    cart.each do |each_piece|
+     each_piece.each {|name,stats| cart_hash[name][:count]+=1}
+       end
+         cart_hash
 end
+
+
+
 
 def apply_coupons(cart, coupons)
 new_cart = {}
 binding.pry
 
-
 end
+
+
+
+
+
+
 
 def apply_clearance(cart)
   # code here
