39b2d2601f0a79242050365a75958f5a3037a29e
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 16:28:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,35 @@
+require "pry"
+
+
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  remove_duplicates = cart.uniq
+  remove_duplicates.each do |each_item|
+    each_item.each {|x,y| cart_hash[x.to_s]=y}
+end
+
+  cart_hash.each {|name, value| cart_hash[name][:count]=0}
+
+ cart.each do |each_piece|
+  each_piece.each {|name,stats| cart_hash[name][:count]+=1}
+  
+ end
+
+ cart_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+new_cart = {}
+cart.collect {|x,y| new_cart[x] = y }
+ coupons.each do |coupon_items|
+   coupon_items.each do |each_coupon_code, each_coupon_value|
+     new_cart[each_coupon_value +" /WCOUPON"]= {each_coupon_code => each_coupon_value}
+    
+     
+  end
+ 
+ end
+
 end
 
 def apply_clearance(cart)
