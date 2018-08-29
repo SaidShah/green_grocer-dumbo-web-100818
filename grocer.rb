9b629c6e8dbe27c9117f3fc2df0576c7fb4d1deb
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 17:43:50 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ new_cart.merge!(cart)
       cart.each do |cart_item, item_values|
         if new_cart.include?("#{values} W/COUPON")
           new_cart["#{values} W/COUPON"][:count]+=1
-      end
+          
         if cart_item == values && !new_cart.include?("#{values} W/COUPON")
           new_cart["#{values} W/COUPON"]={ price: each_coupon[:cost],clearance: true ,count: each_coupon[:num] }
           if new_cart[values][:count] >= each_coupon[:num]
