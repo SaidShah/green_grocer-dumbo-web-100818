50b976874f50976add04bdb65ea38952f953ee78
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 18:54:51 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,7 @@ def apply_clearance(cart)
 cart.each do |cart_item, item_value|
   item_value.each do |field, value|
     if field == :clearance && item_value[:clearance]==true 
-        item_value[:price] = calc_discount(item_value[:price])
+        item_value[:price] = 
 end
  end
   end
@@ -72,7 +72,7 @@ end
 
 
 def calc_discount(num_given)
-(num_given-(num_given*0.20)).to_f 
+(item_value[:price]-(item_value[:price]*0.20)).to_f 
 end
 
 
